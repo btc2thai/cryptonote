57340cd6a0cd00eee5623f2cf99c007b7776676c
btc2thai <35058719+btc2thai@users.noreply.github.com>
2018-01-03 18:43:47 +0900

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -69,7 +69,7 @@ const char     CRYPTONOTE_BLOCKSCACHE_FILENAME[]             = "blockscache.dat"
 const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
 const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
 const char     CRYPTONOTE_BLOCKCHAIN_INDICES_FILENAME[]      = "blockchainindices.dat";
-const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
+const char     MINER_CONFIG_FILE_NAME[]                      = "ezmcoin";
 } // parameters
 
 //TODO Put here the name of your currency
